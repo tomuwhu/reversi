e468c0c77e3a521c42f02b305acef671ad665b16
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-09-21 11:13:31 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: 'reversi',
   build: {
     outDir: 'docs',
     emptyOutDir: true, // also necessary
